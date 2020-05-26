50c3dbd71343be41fe0aa97733e67b9cf303bb59
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-26 13:54:07 +0530

    fix: sleep function

--- misc.h
+++ misc.h
@@ -2,22 +2,16 @@
 #ifndef MISC_H
 #define MISC_H
 
-#define WIN32_LEAN_AND_MEAN
-//omits the extra stuff not used in this project from being included in the application
-#include <windows.h>
-#include <time.h>
+#include <chrono>
+#include <thread>
+#include <iostream>
 class misc
 {
     /**class prototype for miscellaneous functions   */
 public:
-    void sleepFor(int n){
-
-
-    /** sleeps for n milliseconds */
-    clock_t goal = n + clock();
-    while(goal > clock());
-
+    void sleepFor(int n)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(n));
     }
-
 };
 #endif // MISC_H
